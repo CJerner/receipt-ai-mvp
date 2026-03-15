904a636bb94e14b26e69815639df64bac7296967
CJerner <ch_t1@hotmail.com>
2026-03-15 08:52:53 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'finivo-v1';
+const OFFLINE_ASSETS = ['/', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(OFFLINE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Lad API kald gå igennem uden cache
+  if (e.request.url.includes('/api/')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
